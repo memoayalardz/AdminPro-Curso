003deecf054c9c520bd3079c16d3f622906a5098
luisayalardz <lguillermoayalardz@gmail.com>
2020-11-19 15:17:40 -0600

    Fin seccion 7

---
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from 'src/app/services/settings.service';
+
+@Component({
+  selector: 'app-account-settings',
+  templateUrl: './account-settings.component.html',
+  styles: [
+  ]
+})
+export class AccountSettingsComponent implements OnInit {
+
+   public linkTheme = document.querySelector('#theme');
+   public links;
+  constructor(private settingsService : SettingsService) { }
+
+  ngOnInit(): void {
+   
+    this.settingsService.checkCurrentTheme();
+  }
+  changeTheme(theme:string){
+    this.settingsService.changeTheme(theme);
+  }
+ 
+
+}
---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class SidebarService {
+  menu : any[] = [
+    {
+    titulo: 'Dashboard',
+    icono:'mdi mdi-gauge',
+    subMenu:[
+      {titulo:'Main',url:'/'},
+      {titulo:'ProgressBar',url:'progress'},
+      {titulo:'Graficas',url:'grafica1'},
+    
+    ]
+  },
+  
+];
+
+
+constructor() { }
+
+}
